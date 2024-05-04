c7e73ed36905b6cdce0fdb0d78598743a57990a7
alihanayd <aaydin184@posta.pau.edu.tr>
2024-05-04 20:07:39 +0300

    Add fallback

---
+++ src/app/dashboard/components/trend-fallback.tsx
@@ -0,0 +1,18 @@
+import Skeleton from "@/components/skeleton";
+
+export default function TrendFallback() {
+  return (
+    <div className="space-y-5 w-3/5 lg:w-5/6">
+      <div>
+        <Skeleton className="" />
+      </div>
+      <div className="mb-2">
+        <Skeleton className="" />
+      </div>
+      <div className="flex space-x-2">
+        <Skeleton className="" />
+        <Skeleton className="" />
+      </div>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,29 @@
+import { Suspense } from "react";
+import TransactionList from "./components/transaction-list";
+import TransactionListFallback from "./components/transaction-list-fallback";
+import Trend from "./components/trend";
+import TrendFallback from "./components/trend-fallback";
+
 export default function Page() {
   return (
     <>
-      <h1>Page</h1>
+      <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
+        <Suspense fallback={<TrendFallback />}>
+          <Trend type="Income" />
+        </Suspense>
+        <Suspense fallback={<TrendFallback />}>
+          <Trend type="Expense" />
+        </Suspense>
+        <Suspense fallback={<TrendFallback />}>
+          <Trend type="Investment" />
+        </Suspense>
+        <Suspense fallback={<TrendFallback />}>
+          <Trend type="Saving" />
+        </Suspense>
+      </section>
+      <Suspense fallback={<TransactionListFallback />}>
+        <TransactionList />
+      </Suspense>
     </>
   );
 }
