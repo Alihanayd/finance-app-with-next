d2dbe0c68be9f677a5d0ed57059748aef1c7ac20
alihanayd <aaydin184@posta.pau.edu.tr>
2024-05-04 20:09:01 +0300

    Fix style

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <PageHeader className="my-8" />
       <main className="space-y-8">{children}</main>
-      <footer>Footer</footer>
+      <footer className="mt-auto text-center py-8">Footer</footer>
     </>
   );
 }
