a67fbd06a049474ac9a54b7d5eb17229ca0039e7
alihanayd <aaydin184@posta.pau.edu.tr>
2024-05-04 20:12:54 +0300

    feat: Add dark mode toggle to page header

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import DarkModeToggle from "./dark-mode-toggle";
+import useServerDarkMode from "@/hooks/use-server-dark-mode";
 
 export default function PageHeader({ className }: { className: string }) {
+  const theme = useServerDarkMode();
   return (
     <header className={`flex justify-between items-center ${className}`}>
       <Link
@@ -11,7 +14,7 @@ export default function PageHeader({ className }: { className: string }) {
       </Link>
 
       <div className="flex items-center space-x-4">
-        <div>Mode Toggle</div>
+        <DarkModeToggle defaultMode={theme} />
         <div>User Dropdown</div>
       </div>
     </header>
